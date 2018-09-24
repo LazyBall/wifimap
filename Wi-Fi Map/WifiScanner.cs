3847bf6a25e4310c762befc000fd401efd97c948
Alexander <46029218+LazyBall@users.noreply.github.com>
2018-09-24 20:37:40 +0300

    Class WifiScanner created

---
+++ Wi-Fi Map/WifiScanner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.WiFi;
+
+namespace Wi_Fi_Map
+{
+    class WifiScanner
+    {
+        public WiFiAdapter WiFiAdapter { get; private set; }
+
+        private async Task InitializeFirstAdapter()
+        {
+            var access = await WiFiAdapter.RequestAccessAsync();
+
+            if (access != WiFiAccessStatus.Allowed)
+            {
+                throw new Exception("WiFiAccessStatus not allowed");
+            }
+            else
+            {
+                var wifiAdapterResults = await DeviceInformation.
+                  FindAllAsync(WiFiAdapter.GetDeviceSelector());
+
+                if (wifiAdapterResults.Count >= 1)
+                {
+                    this.WiFiAdapter = await WiFiAdapter.FromIdAsync(
+                      wifiAdapterResults[0].Id);
+                }
+                else
+                {
+                    throw new Exception("WiFi Adapter not found.");
+                }
+            }
+        }
+
+        public async Task ScanForNetworks()
+        {
+            if (this.WiFiAdapter != null)
+            {
+                await this.WiFiAdapter.ScanAsync();
+            }
+        }
+
+
+    }
+}
