53c5652bfc2268f05d54fb0ba53e9bb7d9b96d61
LazyBall <LazyBall@localhost>
2018-12-05 01:55:53 +0300

    ????????? ????? Database, ?????? ?????????? ????????? ? ?? ???????? ???????. ??? ?? ??????? ??????????? ??????, ?????? ????????????.

--- Wi-Fi Map/WiFiSignalWithGeoposition.cs
+++ Wi-Fi Map/WiFiSignalWithGeoposition.cs
@@ -5,11 +5,24 @@ namespace Wi_Fi_Map
     public class WiFiSignalWithGeoposition : WiFi
     {
 
-        //public DateTimeOffset TimeStamp { get; set; }
-
         public double Latitude { get; set; }
 
         public double Longitude { get; set; }
+
+        public WiFiSignalWithGeoposition()
+        {
+
+        }
+
+        public WiFiSignalWithGeoposition(WiFiSignal signal, double latitude, double longitude)
+        {
+            BSSID = signal.BSSID;
+            SSID = signal.SSID;
+            SignalStrength = signal.SignalStrength;
+            Encryption = signal.Encryption;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
 
     }
 }
