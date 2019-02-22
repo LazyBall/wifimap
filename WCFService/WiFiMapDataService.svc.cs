6d005bfe9c53b23153c7b13290cf77ee1a8b35b7
Alexander <46029218+LazyBall@users.noreply.github.com>
2019-02-22 15:26:02 +0300

    ??????? ?????????? ???????

---
+++ WCFService/WiFiMapDataService.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class WiFiMapDataService : IWiFiMapDataService
+    {
+        public IEnumerable<WiFiSignalWithGeoposition> GetAllData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public WiFiSignalWithGeoposition GetDataUsingDataContract(WiFiSignalWithGeoposition composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
